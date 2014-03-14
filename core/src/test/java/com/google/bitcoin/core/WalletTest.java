8d8b85eb0810dc1ccf2ed1882540a4cbb1dff199
Mike Hearn <mike@plan99.net>
2014-03-14 14:31:59 +0100

    Wallet: keychain migration, part 1
    Switch the existing code to using only a single BasicKeyChain. Rework the encryption API's a bit as part of doing this. Disable key rotation for now as it will have to change in the HDW world anyway.
    
    Functionality wise, this is largely a no-op, though BasicKeyChain should be faster at handling lots of keys at once.

--- core/src/test/java/com/google/bitcoin/core/WalletTest.java
+++ core/src/test/java/com/google/bitcoin/core/WalletTest.java
@@ -18,8 +18,6 @@ package com.google.bitcoin.core;
 
 import com.google.bitcoin.core.Transaction.SigHash;
 import com.google.bitcoin.core.Wallet.SendRequest;
-import com.google.bitcoin.wallet.DefaultCoinSelector;
-import com.google.bitcoin.wallet.RiskAnalysis;
 import com.google.bitcoin.crypto.KeyCrypter;
 import com.google.bitcoin.crypto.KeyCrypterException;
 import com.google.bitcoin.crypto.KeyCrypterScrypt;
@@ -29,18 +27,17 @@ import com.google.bitcoin.utils.MockTransactionBroadcaster;
 import com.google.bitcoin.utils.TestUtils;
 import com.google.bitcoin.utils.TestWithWallet;
 import com.google.bitcoin.utils.Threading;
-import com.google.bitcoin.wallet.KeyTimeCoinSelector;
-import com.google.bitcoin.wallet.WalletFiles;
-import com.google.bitcoin.wallet.WalletTransaction;
+import com.google.bitcoin.wallet.*;
 import com.google.bitcoin.wallet.WalletTransaction.Pool;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.ByteString;
 import org.bitcoinj.wallet.Protos;
-import org.bitcoinj.wallet.Protos.ScryptParameters;
 import org.bitcoinj.wallet.Protos.Wallet.EncryptionType;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,10 +45,14 @@ import org.spongycastle.crypto.params.KeyParameter;
 import org.spongycastle.util.encoders.Hex;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.security.SecureRandom;
-import java.util.*;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -1304,18 +1305,11 @@ public class WalletTest extends TestWithWallet {
 
         // Wallet should now be unencrypted.
         assertTrue("Wallet is not an unencrypted wallet", encryptedWallet.getKeyCrypter() == null);
-
-        // Correct password should not decrypt first encrypted private key as wallet is unencrypted.
-        assertTrue("checkPassword result is wrong with correct password", !encryptedWallet.checkPassword(PASSWORD1));
-
-        // Incorrect password should not decrypt first encrypted private key as wallet is unencrypted.
-        assertTrue("checkPassword result is wrong with incorrect password", !encryptedWallet.checkPassword(WRONG_PASSWORD));
-
-        // Encrypt wallet.
-        encryptedWallet.encrypt(keyCrypter, aesKey);
-
-        // Wallet should now be of type WalletType.ENCRYPTED_SCRYPT_AES.
-        assertTrue("Wallet is not an encrypted wallet", encryptedWallet.getEncryptionType() == EncryptionType.ENCRYPTED_SCRYPT_AES);
+        try {
+            encryptedWallet.checkPassword(PASSWORD1);
+            fail();
+        } catch (IllegalStateException e) {
+        }
     }
 
     @Test
@@ -1369,14 +1363,14 @@ public class WalletTest extends TestWithWallet {
     @Test(expected = KeyCrypterException.class)
     public void addUnencryptedKeyToEncryptedWallet() throws Exception {
         ECKey key1 = new ECKey();
-        encryptedWallet.addKey(key1);
+        encryptedWallet.importKey(key1);
     }
 
     @Test(expected = KeyCrypterException.class)
     public void addEncryptedKeyToUnencryptedWallet() throws Exception {
         ECKey key1 = new ECKey();
         key1 = key1.encrypt(keyCrypter, keyCrypter.deriveKey("PASSWORD!"));
-        wallet.addKey(key1);
+        wallet.importKey(key1);
     }
 
     @Test
@@ -1389,21 +1383,24 @@ public class WalletTest extends TestWithWallet {
         byte[] salt = new byte[KeyCrypterScrypt.SALT_LENGTH];
         secureRandom.nextBytes(salt);
         Protos.ScryptParameters.Builder scryptParametersBuilder = Protos.ScryptParameters.newBuilder().setSalt(ByteString.copyFrom(salt));
-        ScryptParameters scryptParameters = scryptParametersBuilder.build();
+        Protos.ScryptParameters scryptParameters = scryptParametersBuilder.build();
 
         KeyCrypter keyCrypterDifferent = new KeyCrypterScrypt(scryptParameters);
 
         ECKey ecKeyDifferent = new ECKey();
         ecKeyDifferent = ecKeyDifferent.encrypt(keyCrypterDifferent, aesKey);
         assertEquals("Wrong number of keys in wallet before key addition", 1, encryptedWallet.getKeychainSize());
+    }
 
-        try {
-            encryptedWallet.importKey(ecKeyDifferent);
-            fail("AddKey should have thrown an EncrypterDecrypterException but did not.");
-        } catch (KeyCrypterException ede) {
-            // Expected behaviour.
-        }
-        assertEquals("Wrong number of keys in wallet before key addition", 1, encryptedWallet.getKeychainSize());
+    @Test
+    public void importAndEncrypt() throws IOException, InsufficientMoneyException {
+        final ECKey key = new ECKey();
+        encryptedWallet.importKeysAndEncrypt(ImmutableList.of(key), PASSWORD1);
+        sendMoneyToWallet(encryptedWallet, Utils.COIN, key.toAddress(params), AbstractBlockChain.NewBlockType.BEST_CHAIN);
+        assertEquals(Utils.COIN, encryptedWallet.getBalance());
+        SendRequest req = Wallet.SendRequest.emptyWallet(new ECKey().toAddress(params));
+        req.aesKey = encryptedWallet.getKeyCrypter().deriveKey(PASSWORD1);
+        encryptedWallet.sendCoinsOffline(req);
     }
 
     @Test
@@ -2146,6 +2143,7 @@ public class WalletTest extends TestWithWallet {
         assertEquals(outputValue, request.tx.getOutput(0).getValue());
     }
 
+    @Ignore("Key rotation temporarily disabled during HD wallet migration")
     @Test
     public void keyRotation() throws Exception {
         // Watch out for wallet-initiated broadcasts.
